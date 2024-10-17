d96fa86654a422e126d5af7f0543dbcf3a99ab09
yiru07 <yiruliu0715@gmail.com>
2024-10-17 16:57:35 +1100

    Change http to hrrps

--- NoMash-Library/src/views/WeatherView.vue
+++ NoMash-Library/src/views/WeatherView.vue
@@ -41,12 +41,12 @@ export default {
     computed: {
         temperature() {
             return this.weatherData
-                ? Math.floor(this.weatherData.main.temp - 273.15) // 转换为摄氏度
+                ? Math.floor(this.weatherData.main.temp - 273.15) 
                 : null;
         },
         iconUrl() {
             return this.weatherData
-                ? `http://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+                ? `https://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
                 : null;
         },
     },
@@ -58,7 +58,7 @@ export default {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(async (position) => {
                     const { latitude, longitude } = position.coords;
-                    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+                    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
                     await this.fetchWeatherData(url);
                 });
             }
@@ -73,7 +73,7 @@ export default {
         },
         async searchByCity() {
             if (this.city.trim()) { 
-                const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`;
+                const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`;
                 await this.fetchWeatherData(url);
             }
         }
